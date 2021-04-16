f68ec2ff15cb9c96de71ddc66be4fef6ad0bbcb9
Sneha Jaikumar <snehaj@email.unc.edu>
2021-04-16 15:08:24 -0400

    Updated readme, created a few plots

--- HeartFailureProject.R
+++ HeartFailureProject.R
@@ -7,3 +7,11 @@ library(tidyverse)
 library(readr)
 
 heartFailure <- read_csv("heart_failure_clinical_records_dataset.csv")
+typeof(heartFailure)
+names(heartFailure)
+
+plot(serum_creatinine~smoking, data = heartFailure)
+plot(ejection_fraction~smoking, data = heartFailure)
+plot(creatinine_phosphokinase~high_blood_pressure, data = heartFailure)
+plot(creatinine_phosphokinase~DEATH_EVENT, data = heartFailure)
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CoolDataScientists
+
+Team Name: The Cool Data Scientists
+Group: Sneha Jaikumar
+
+Our heart failure prediction dataset consists of 12 features that can be analyzed to predict mortality by heart failure so patients can receive proper care early on.
