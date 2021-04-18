92370d229bf089f1e506f12857e51d1e2dccf085
Sarthi Patil <69725719+sarthi-patil@users.noreply.github.com>
2021-04-17 21:28:39 -0400

    Update HeartFailureProject.R

--- HeartFailureProject.R
+++ HeartFailureProject.R
@@ -8,15 +8,15 @@ library(readr)
 
 heartFailure <- read_csv("heart_failure_clinical_records_dataset.csv")
 typeof(heartFailure)
-class(heart_failure)
+class(heartFailure)
 names(heartFailure)
-ncol(heart_failure)
-nrow(heart_failure)
-length(heart_failure$anaemia)
-filtered <- filter(heart_failure, diabetes == 1)
+ncol(heartFailure)
+nrow(heartFailure)
+length(heartFailure$anaemia)
+filtered <- filter(heartFailure, diabetes == 1)
 filtered
-select(heart_failure, serum_creatinine, high_blood_pressure)
-arrange(heart_failure, platelets)
+select(heartFailure, serum_creatinine, high_blood_pressure)
+arrange(heartFailure, platelets)
 
 
 plot(serum_creatinine~smoking, data = heartFailure)
@@ -24,8 +24,8 @@ plot(ejection_fraction~smoking, data = heartFailure)
 plot(creatinine_phosphokinase~high_blood_pressure, data = heartFailure)
 plot(creatinine_phosphokinase~DEATH_EVENT, data = heartFailure)
 
-plot(heart_failure$age, heart_failure$platelets)
-model.lm <- lm(platelets~age, data = heart_failure)
+plot(heartFailure$age, heartFailure$platelets)
+model.lm <- lm(platelets~age, data = heartFailure)
 plot(model.lm, which = 1:2)
      #Plot of simple linear regression model between the platelets and age variables in the dataset. The Q-Q plot shows the residuals are close to the line of normality, but the first plot shows that many of the residuals are far from zero.
   
